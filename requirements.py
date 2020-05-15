1602fb673fb9e171b94aea1330f25a59a57c5560
Symphanny <symphannysec@protonmail.com>
2020-05-14 20:40:50 -0400

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,4 +1,4 @@
 import os
 print("We are installing everything you need!!")
-os.system("apt install python3-pip -y;pip3 install colorama;pip3 install requests")
+os.system("apt install python3-pip -y;pip3 install colorama;pip3 install requests;apt install mysql-client -y;apt install nmap -y")
 print("Ok! Installed finish")
